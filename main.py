7ab2c764e20bd49b01682f8bd159145024564d72
bznerd <ben.zemp.c@gmail.com>
2024-05-15 10:24:39 -0400

    Working base physics

--- main.py
+++ main.py
@@ -1,2 +1,53 @@
 from modules import sim, physics
-print("Hooray")
+from time import sleep
+from matplotlib import pyplot as plt
+import numpy as np
+
+mass = 200 # kg
+drag_coeff = 0
+drag_area = 0
+lift_coeff = 0
+wing_area = 0
+wheel_base = 1.7 # m
+wheel_radius = 0.26 # m 
+coeff_friction = 1.3 # unitless
+w_free = 120 # rad/s
+V_max = 200 # Volts
+T_stall = 200 # Nm
+I_stall = 180 # A
+
+dc_motor = physics.DC_Motor(w_free, V_max, T_stall, I_stall)
+racecar = physics.RaceCar(mass, drag_coeff, drag_area, lift_coeff, wing_area, wheel_base, wheel_radius, coeff_friction, dc_motor)
+
+t = 0
+step_size = 0.02
+sim_interval = 0.02
+control = np.array([200, 0, 0])
+position_x = [0]
+position_y = [0]
+velocity = [0]
+lateral_velocity = [0]
+time = [t]
+
+for i in range(3000):
+    x_current = racecar.get_state()
+    t_current = t
+
+    control = np.array([200, 0, 0.0005 * t])
+
+    x_next, t_next = sim.rk4(x_current, control, t_current, racecar.dxdt, step_size, t_current + sim_interval)
+    #print(x_next)
+    racecar.update_state(x_next)
+    t = t_next
+    position_x.append(x_next[0])
+    position_y.append(x_next[1])
+    velocity.append(np.linalg.norm(x_next[2:4]))
+    heading = np.array([-np.sin(x_next[4]), np.cos(x_next[4])])
+    lateral_velocity.append(np.linalg.norm(heading.dot(x_next[2:4]) * heading))
+    time.append(t)
+
+#plt.plot(position_x, position_y, label="Position (m)")
+plt.plot(time, velocity, label="Velocity (m/s)")
+plt.plot(time, lateral_velocity, label="Lateral velocity (m/s)")
+plt.legend()
+plt.show()
--- modules/physics.py
+++ modules/physics.py
@@ -1,4 +1,11 @@
 import numpy as np
+
+# Constants
+air_density = 1
+transition_coefficient = 0.01 # unitless
+transition_coefficient = 0.006 # unitless
+g = 9.81 # m/s^2
+
 # Drag force
 # takes velocity vector, fluid density rho, drag coefficient and frontal surface area
 def drag_force(velocity, rho, drag_coeff, area):
@@ -15,4 +22,83 @@ def lift_force(velocity, rho, coeff_lift, area):
     direction = np.array([[0,0,1]])
     # F_lift = 1/2 C_l*p*A*V^2 directed perpendicular to velocity
     return direction * (1/2 * coeff_lift * rho * area * velocity**2)
+
+
+# I'm fucking quitting
+def static_to_kinetic_transition(F_static, F_kinetic, F_limit, transition_coefficient):
+    force_mag = np.linalg.norm(F_static)
+    transition_ratio = (1 + np.e ** (1/transition_coefficient - force_mag/(transition_coefficient * F_limit)))**(-1)
+    return (1 - transition_ratio) * F_static + transition_ratio * F_kinetic, transition_ratio
     
+
+# Torque from a dc motor
+# takes motor angular speed w, applied voltage V_app, free speed w_free, max voltage V_max, stall torque T_stall, and stall current I_stall
+def dc_motor_torque(w, V_app, w_free, V_max, T_stall, I_stall):
+    K_s = T_stall/I_stall
+    R_m = V_max/I_stall
+    V_emf = w/w_free * V_max
+
+    I_stator = (V_app - V_emf)/R_m
+    return I_stator * K_s
+
+class DC_Motor:
+    def __init__(self, w_free, V_max, T_stall, I_stall):
+        self.w_free = w_free
+        self.V_max = V_max
+        self.T_stall = T_stall
+        self.I_stall = I_stall
+
+    def calculate_torque(self, w, V_app):
+        return dc_motor_torque(w, V_app, self.w_free, self.V_max, self.T_stall, self.I_stall)
+
+# Collection of all the parameters, state, and dynamics of the simulated race car 
+# State vector is x,y position, x,y velocity, and the car heading
+class RaceCar:
+    def __init__(self, mass, drag_coeff, drag_area, coeff_lift, wing_area, wheel_base, wheel_radius, coeff_friction, dc_motor):
+        self.mass = mass # kg
+        self.drag_coeff = drag_coeff # 
+        self.drag_area = drag_area
+        self.coeff_lift = coeff_lift
+        self.wing_area = wing_area
+        self.wheel_base = wheel_base # m
+        self.wheel_radius = wheel_radius # m
+        self.coeff_friction = coeff_friction
+        self.dc_motor = dc_motor
+        self.state = np.zeros(5)
+
+    def steering_angle_to_turn_radius(self, steering_angle):
+        return np.sqrt((self.wheel_base/2)**2 + self.wheel_base**2 * np.tan(steering_angle)**(-2))
+
+    def dxdt(self, x, u, t):
+        velocity = x[2:4] 
+        heading = x[4]
+
+        heading_vector = np.array([np.cos(heading), np.sin(heading)])
+        if u[2] != 0:
+            inward_normal = np.array([-1 * np.sin(heading), np.cos(heading)]) if u[2] > 0 else np.array([np.sin(heading), -1 * np.cos(heading)])
+        else: inward_normal = np.zeros(2)
+        forward_motion = velocity.dot(heading_vector) * heading_vector
+
+        wheel_speed = np.linalg.norm(forward_motion)/self.wheel_radius
+        motor_force = self.dc_motor.calculate_torque(wheel_speed, u[0])/self.wheel_radius * heading_vector
+        braking_force = -1 * u[1] * heading_vector 
+
+        turn_radius = self.steering_angle_to_turn_radius(u[2])
+        centripetal_acceleration = np.linalg.norm(forward_motion)**2 / turn_radius
+        turning_force = centripetal_acceleration * self.mass * inward_normal
+
+        static_friction = motor_force + braking_force + turning_force
+        kinetic_friction = -1 * velocity/np.linalg.norm(velocity) * g * self.mass if np.linalg.norm(velocity) != 0 else np.zeros(2)
+        limit_of_friction = g * self.mass
+        frictional_force, transition_factor = static_to_kinetic_transition(static_friction, kinetic_friction, limit_of_friction, transition_coefficient)
+        acceleration = frictional_force / self.mass
+        angular_velocity = np.linalg.norm(forward_motion)/turn_radius * np.sign(u[2])
+
+        return np.array([velocity[0], velocity[1], acceleration[0], acceleration[1], angular_velocity])
+        
+
+    def update_state(self, new_state):
+        self.state = new_state
+
+    def get_state(self):
+        return self.state
